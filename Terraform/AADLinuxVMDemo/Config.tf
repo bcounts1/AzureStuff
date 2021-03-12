34e733ba3419aec64005643b9e6438e9fb66dff7
Bryan Counts <b.counts@gmail.com>
2021-03-11 21:52:06 -0500

    BGC - Add AAD Linux Demo

---
+++ Terraform/AADLinuxVMDemo/Config.tf
@@ -0,0 +1,135 @@
+terraform {
+  required_providers {
+    azurerm = "~> 2.50"
+  }
+}
+
+provider "azurerm" {
+  subscription_id       = var.subscription_id
+  client_id             = var.client_id
+  client_secret         = var.client_secret
+  tenant_id             = var.tenant_id
+  features {}
+}
+
+resource "tls_private_key" "example" {
+    algorithm = "RSA"
+    rsa_bits  = 4096
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.RGname
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "AADlinuxVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "Default"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_subnet" "bastion" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.3.0/27"]
+}
+
+resource "azurerm_public_ip" "vmpip" {
+  name = "AADLinux-pip"
+  location = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method = "Dynamic"
+  sku = "Basic"
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "AADLinux-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.vmpip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "AADLinux-machine"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+ # admin_password      = "adlfkjwADFe6"
+  disable_password_authentication = true
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+    admin_ssh_key {
+    username   = "adminuser"
+    public_key = tls_private_key.example.public_key_openssh
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_virtual_machine_extension" "adextenstion" {
+    name                       = "AADLoginForLinux"
+    virtual_machine_id         = azurerm_linux_virtual_machine.example.id
+    publisher                  = "Microsoft.Azure.ActiveDirectory.LinuxSSH"
+    type                       = "AADLoginForLinux"
+    type_handler_version       = "1.0"
+    auto_upgrade_minor_version = true
+
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "InternalSubnetNSG"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "inboundAllow"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
+output "tls_private_key" {
+    value = tls_private_key.example.private_key_pem
+}
+output "public_ip_address" {
+    value = azurerm_public_ip.vmpip.ip_address
+}
---
+++ Terraform/AADLinuxVMDemo/README.md
@@ -0,0 +1,12 @@
+# AAD Linux VM Authentication Demo
+
+The purpose of this demo is to easily experiment with the **preview** AAD authentication feature for Linux VMs. You can find more information on this feature [here](https://docs.microsoft.com/en-us/azure/virtual-machines/linux/login-using-aad).
+
+## Whats deployed in this demo?
+
+1x Vnet
+2x Subnets
+1x Public IP address
+1x Linux VM
+1x NSG
+1x RSA key
